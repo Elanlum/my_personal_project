9eec3f90adc40cefe573f2b304a9191f12ac1c8e
Elanlum <piratone94@mail.ru>
2018-08-07 21:43:56 +0300

    Test apple

---
+++ geometry_task/src/main/java/Test/AppleTest.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class AppleTest {
+    public static void main(String[] args) {
+        Fruit apple = new Fruit();
+        System.out.println("Вес яблока равен: " + apple.appleWeight);
+        System.out.println("Цвет яблока: " + apple.color);
+    }
+}
---
+++ geometry_task/src/main/java/Test/Fruit.java
@@ -0,0 +1,18 @@
+package Test;
+
+public class Fruit {
+     protected int appleWeight = 10;
+     String color = "green";
+
+    public int getWeight(){
+        return appleWeight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    //    public static void main(String[] args) {
+//        System.out.println("Вес яблока равен: " + appleWeight);
+//    }
+}
